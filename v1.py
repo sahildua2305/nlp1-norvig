d53e8660faa14b7833cbcd6035868664141c47a6
Sahil <sahildua2305@gmail.com>
2014-07-18 12:38:38 +0530

    a less accurate own version of spell corrector

--- v1.py
+++ v1.py
@@ -1,6 +1,10 @@
+import re, collections
+
+words = re.findall('[a-z]+', open('big.txt').read().lower())
+
 file = open('spell-errors.txt', 'r')
 
-dic = {}
+model = collections.defaultdict(lambda: 0)
 
 for line in file:
     line = line.replace('\n', '')
@@ -8,6 +12,14 @@ for line in file:
     correct_word = lis[0]
     wrong_words = lis[1].split(', ')
     for wrong_word in wrong_words:
-        dic[wrong_word] = correct_word
+        model[wrong_word] = correct_word
 
 file.close()
+
+while(1):
+    w = raw_input()
+    if w=='': break
+    if w in words or model[w] == 0:
+        print w
+    else:
+        print model[w]
