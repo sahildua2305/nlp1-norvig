81c60361a46cf094e1480b7d6361148921510f2d
Sahil <sahildua2305@gmail.com>
2014-07-18 13:16:56 +0530

    added more operations of edit-distance acc to norvig's implementation

--- v1.py
+++ v1.py
@@ -24,7 +24,9 @@ def edits1(word):
     splits     = [(word[:i], word[i:]) for i in range(len(word)+1)]
     deletes    = [a + b[1:] for a, b in splits if b]
     transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-    return set(deletes + transposes)
+    replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+    inserts    = [a + c + b for a, b in splits for c in alphabet]
+    return set(deletes + transposes + replaces + inserts)
 
 def known(words):
     return set(w for w in words if w in nwords)
